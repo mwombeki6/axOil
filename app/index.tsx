cc717755f58c5bfabe5e33ed45f27ca16fbc8107
mwombeki6 <hackerlubex@protonmail.com>
2025-04-06 14:16:51 +0300

    added themes

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl font-bold text-blue-600">Welcome!</Text>
+      <Text className="text-5xl font-bold text-light-100">Welcome!</Text>
     </View>
   );
 }
