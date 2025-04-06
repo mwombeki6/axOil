9d61ed015acaac5db95968ed4accb149b9b0feec
mwombeki6 <hackerlubex@protonmail.com>
2025-04-06 14:09:45 +0300

    refactor first

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {},
+        accentColor: "#AB8BFF"
+      }
+
+    },
+  },
+  plugins: [],
+}
